06c35637e8c54793ede468427ba480fc8aa2c02b
Anders Sjöberg <anderssjoeberg75@gmail.com>
2026-07-21 07:33:25 +0200

    Fix BACKEND_URL in start-freja.bat to default to localhost

--- start-freja.bat
+++ start-freja.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "%~dp0"
-set BACKEND_URL=http://192.168.107.15:8000
+if "%BACKEND_URL%"=="" set BACKEND_URL=http://localhost:8000
 start "FREJA Client" /min "%CD%\venv\Scripts\python.exe" run_client.py
+
